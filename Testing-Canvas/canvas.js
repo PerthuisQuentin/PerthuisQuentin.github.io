9cd947788600a13daf3c33db78383f01f7573ba9
Quentin Perthuis <perthuis.quentin@gmail.com>
2016-08-07 19:20:46 +0200

    Add Testing-Canvas

---
+++ Testing-Canvas/canvas.js
@@ -0,0 +1,112 @@
+window.onload = function()
+{
+	ResizeCanvas();
+	
+	// Récupération du canvas et du context
+	var canvas = document.getElementById('mon_canvas');
+	if(!canvas) {
+		alert("Impossible de récupérer le canvas");
+		return;
+	}
+
+	var context = canvas.getContext('2d');
+	if(!context) {
+		alert("Impossible de récupérer le context du canvas");
+		return;
+	}
+
+	// Définition d'un objet représentant une balle
+	function Ball() {
+		this.x = 0; this.y = 0;
+		this.dir = 0;
+		this.speed = 0;
+		this.radius = 1;
+		this.color = "#000000";
+		
+		// Initialisation des variables de la balle
+		this.Init = function(x, y, dir, speed, radius, color) {
+			this.x = x;
+			this.y = y;
+			this.dir = dir;
+			this.speed = speed;
+			this.radius = radius;
+			this.color = color;
+		};
+		
+		// Desinne la balle dans le canvas
+		this.Draw = function(context) {
+			// Dessine un cercle
+			context.fillStyle = this.color;
+			context.beginPath();
+			context.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+			context.fill();
+			context.closePath();
+		};
+		
+		// Mets à jour les variables de la balle
+		this.Update = function() {
+			// Anticipation de la nouvelle position
+			var newX = this.x + Math.cos(this.dir) * this.speed;
+			var newY = this.y + Math.sin(this.dir) * this.speed;
+			
+			// Rebonds sur les bordures externes
+			if ((newX > (canvas.width - this.radius)) || (newX < this.radius)) {
+				if(this.dir > 0)
+					this.dir = Math.PI - this.dir;
+				else if(this.dir < 0)
+					this.dir = -Math.PI - this.dir;
+				else if(this.dir == 0) 
+					this.dir = Math.PI;
+				else if(this.dir == Math.PI)
+					this.dir = 0;
+			}
+			else if ((newY > (canvas.height - this.radius)) || (newY < this.radius)) {
+				this.dir = -this.dir;
+			}
+			
+			// Mise à jour de la position
+			this.x += Math.cos(this.dir) * this.speed;
+			this.y += Math.sin(this.dir) * this.speed;
+		};
+	}
+	
+    var ball = new Ball();
+	ball.Init(50, 50, GetRandomAngle(), 5, 10, "#0000FF");
+	
+	// Boucle de rafraichissement du canvas
+    Animate();
+
+	// Fonction de rafraichissement du canvas
+    function Animate()
+    {
+		// Clear du canvas
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		
+		// Mise à jour 
+		ball.Update();
+		
+		// Affichage sur le canvas
+		ball.Draw(context);
+		
+		requestAnimationFrame(Animate);
+    } 
+} 
+
+window.onresize = ResizeCanvas;
+
+// Redimensionne le canvas en fonction de la fenêtre
+function ResizeCanvas() {
+	var canvas = document.getElementById('mon_canvas');
+	if(!canvas) {
+		alert("Impossible de récupérer le canvas");
+		return;
+	}
+	
+	canvas.width = window.innerWidth * 0.9;
+	canvas.height = window.innerHeight * 0.8;
+}
+
+// Donne un angle dans l'intervalle ]-PI; PI]
+function GetRandomAngle() {
+	return (Math.random() * 2 * Math.PI - Math.PI) * -1;
+}

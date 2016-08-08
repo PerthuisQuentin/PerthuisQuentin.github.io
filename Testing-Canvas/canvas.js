70e44e5c6407d56b851e9a8d1949ac467046715d
Quentin Perthuis <perthuis.quentin@gmail.com>
2016-08-08 04:52:38 +0200

    Add Duplication mode (Maybe instable)

--- Testing-Canvas/canvas.js
+++ Testing-Canvas/canvas.js
@@ -3,7 +3,7 @@ window.onload = function()
 	ResizeCanvas();
 	
 	// Récupération du canvas et du context
-	var canvas = document.getElementById('balls_canvas');
+	var canvas = document.getElementById('balls-canvas');
 	if(!canvas) {
 		alert("Impossible de récupérer le canvas");
 		return;
@@ -14,9 +14,31 @@ window.onload = function()
 		alert("Impossible de récupérer le context du canvas");
 		return;
 	}
-
+	
+	var duplicationCheck = document.getElementById('mode-duplication');
+	if(!duplicationCheck) {
+		alert("Impossible de récupérer le checkbox de duplication");
+		return;
+	}
+	
+	var duplicationMode = false;
+	
+	duplicationCheck.addEventListener("change",
+		function(e) {
+			duplicationMode = e.target.checked;
+		}
+	);
+	
+	//Tableau contenant toutes les balles
+	var balls = [];
+	var newBalls = [];
+	var ballsToDelete = [];
+	var nextId = 0;
+	
 	// Définition d'un objet représentant une balle
 	function Ball() {
+		this.id = nextId++;
+		this.original = true;
 		this.x = 0; this.y = 0;
 		this.dir = 0;
 		this.speed = 0;
@@ -24,23 +46,25 @@ window.onload = function()
 		this.color = "#000000";
 		
 		// Initialisation des variables de la balle
-		this.Init = function(x, y, dir, speed, radius, color) {
+		this.Init = function(x, y, dir, speed, radius, color, original) {
 			this.x = x;
 			this.y = y;
 			this.dir = dir;
 			this.speed = speed;
 			this.radius = radius;
 			this.color = color;
+			this.original = original
 		};
 		
 		// Initialise avec des valeurs aléatoires
 		this.InitRandom = function() {
-			this.radius = Math.floor(Math.random() * (10 - 2)) + 2;
+			this.radius = Math.floor(Math.random() * (30 - 4)) + 4;
 			this.x = Math.floor(Math.random() * (canvas.width - 2*this.radius)) + this.radius;
 			this.y = Math.floor(Math.random() * (canvas.height - 2*this.radius)) + this.radius;
 			this.dir = (Math.random() * 2 * Math.PI - Math.PI) * -1;
 			this.speed = Math.floor(Math.random() * (10 - 2)) + 2;
 			this.color = '#' + Math.floor(Math.random()*16777215).toString(16);
+			this.original = true;
 		};
 		
 		// Desinne la balle dans le canvas
@@ -61,6 +85,8 @@ window.onload = function()
 			
 			// Rebonds sur les bordures externes
 			if ((newX > (canvas.width - this.radius)) || (newX < this.radius)) {
+				if(duplicationMode) this.ReboundDuplication();
+					
 				if(this.dir > 0)
 					this.dir = Math.PI - this.dir;
 				else if(this.dir < 0)
@@ -71,6 +97,7 @@ window.onload = function()
 					this.dir = 0;
 			}
 			else if ((newY > (canvas.height - this.radius)) || (newY < this.radius)) {
+				if(duplicationMode) this.ReboundDuplication();
 				this.dir = -this.dir;
 			}
 			
@@ -78,20 +105,42 @@ window.onload = function()
 			this.x += Math.cos(this.dir) * this.speed;
 			this.y += Math.sin(this.dir) * this.speed;
 		};
+		
+		// Dupplique la balle
+		this.ReboundDuplication= function() {
+			var newRadius = this.radius / 2;
+			if(newRadius < 0.5) {
+				ballsToDelete.push(this.id);
+				if(this.original) {
+					var newBall = new Ball();
+					newBall.InitRandom();
+					newBalls.push(newBall);
+				}
+				return;
+			}
+			
+			this.radius = newRadius;
+			
+			var duplicata = new Ball();
+			var newDir;
+			if(this.dir > 0) newDir = this.dir - Math.PI;
+			else if(this.dir < 0) newDir = this.dir + Math.PI;
+			else newDir = 0;
+			
+			duplicata.Init(this.x, this.y, newDir, this.speed, this.radius, this.color, false);
+			newBalls.push(duplicata);
+		};
 	}
 	
-	//Tableau contenant toutes les balles
-	var balls = [];
-	
 	// Récupération du slider
-	var slider = document.getElementById('balls_slider');
+	var slider = document.getElementById('balls-slider');
 	if(!slider) {
 		alert("Impossible de récupérer le slider");
 		return;
 	}
 	
 	// Récupération de l'input
-	var input = document.getElementById('balls_input');
+	var input = document.getElementById('balls-input');
 	if(!input) {
 		alert("Impossible de récupérer l'input");
 		return;
@@ -106,7 +155,7 @@ window.onload = function()
 			for(var i = 0; i < ballsNeeded; i++) {
 				var ball = new Ball();
 				ball.InitRandom();
-				balls.push(ball);
+				newBalls.push(ball);
 			}
 		}
 		else if(balls.length > sliderValue) {
@@ -128,11 +177,15 @@ window.onload = function()
 	// Evenement au changement de la valeur de l'input
 	input.addEventListener("change",
 		function(e) {
-			setBallAmount(e.target.value);
-			slider.value = e.target.value;
+			var v = Math.round(e.target.value);
+			v = v>200?200:v;
+			v = v<0?0:v;
+			
+			e.target.value = v;
+			setBallAmount(v);
+			slider.value = v;
 		}
 	);
-	
 		
 	// Initialisation des balles
 	setBallAmount(1);
@@ -150,7 +203,14 @@ window.onload = function()
 		for(var b in balls) {
 			balls[b].Update();
 			balls[b].Draw(context);
+			
+			if(ballsToDelete.indexOf(balls[b].id) === -1)
+				newBalls.push(balls[b]);
 		}
+		
+		balls = newBalls;
+		newBalls = [];
+		ballsToDelete = [];
 		
 		requestAnimationFrame(Animate);
     } 
@@ -160,7 +220,7 @@ window.onresize = ResizeCanvas;
 
 // Redimensionne le canvas en fonction de la fenêtre
 function ResizeCanvas() {
-	var canvas = document.getElementById('balls_canvas');
+	var canvas = document.getElementById('balls-canvas');
 	if(!canvas) {
 		alert("Impossible de récupérer le canvas");
 		return;
